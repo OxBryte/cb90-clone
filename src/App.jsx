01cedb6cea8f6d835f85b05a1652db247522e95d
BrightTechnologies1 <silascyrax@gmail.com>
2023-09-03 19:41:55 +0100

    added: trustedby

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Accomplishment from "./components/accomplishment"
 import Layout from "./components/page-layout/layout"
 import Premier from "./components/premier"
 import StandOut from "./components/standout"
+import TrustedBy from "./components/trustedBy"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Layout navbar={true} footer={true} >
       <Premier />
       <Accomplishment />
+      <TrustedBy />
       <StandOut />
     </Layout>
   )
---
+++ src/components/trustedBy/index.jsx
@@ -0,0 +1,15 @@
+import { Box, Heading, Stack } from '@chakra-ui/react'
+import ContainLayout from '../page-layout/container'
+
+export default function TrustedBy() {
+    return (
+        <Box w='full' py='100px'>
+            <ContainLayout>
+                <Stack direction={['column', 'row']} justify='space-between' w='full'>
+                    <Heading>We are trusted by</Heading>
+                    <h2>Images</h2>
+                </Stack>
+            </ContainLayout>
+        </Box>
+    )
+}
