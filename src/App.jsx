eb734fe5fd76b42e4a3c69270fa3c8e6d6a29cfa
BrightTechnologies1 <silascyrax@gmail.com>
2023-09-08 14:40:55 +0100

    update: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Home from "./pages/home"
 import Faqs from "./pages/faqs"
+import About from "./pages/about"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/faqs" element={<Faqs />} />
+          <Route path="/about" element={<About />} />
       </Routes>
     </Router>
     </>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,12 @@
 import { Box, Button, Flex, HStack, IconButton, Image, Link, useBreakpointValue } from "@chakra-ui/react";
 import ContainLayout from "../page-layout/container";
 import { FiMenu } from "react-icons/fi";
-// import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
 
-    // const location = useLocation();
-    // const { pathname } = location;
+    const location = useLocation();
+    const { pathname } = location;
     const isDesktop = useBreakpointValue({ base: false, lg: true })
 
 
@@ -22,19 +22,19 @@ export default function Navbar() {
                         isDesktop ? (
                             <Flex align='center'>
                                 <HStack spacing={10} color='brand.300'>
-                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer'>
+                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer' color={pathname === '/' && 'brand.200'}>
                                         <Box fontSize='14px' as='a' href='/'>Home</Box>
                                     </Box>
-                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer'>
+                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer' color={pathname === '/about' && 'brand.200'}>
                                         <Box fontSize='14px' as='a' href='/about'>About</Box>
                                     </Box>
-                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer'>
+                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer' color={pathname === '/pricing' && 'brand.200'}>
                                         <Box fontSize='14px' as='a' href='/pricing'>Pricing</Box>
                                     </Box>
-                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer'>
+                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer' color={pathname === '/faqs' && 'brand.200'}>
                                         <Box fontSize='14px' as='a' href='/faqs'>Faqs</Box>
                                     </Box>
-                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer'>
+                                    <Box _hover={{ color: 'brand.200' }} cursor='pointer' color={pathname === '/login' && 'brand.200'}>
                                         <Box fontSize='14px' as='a' href='/'>Login</Box>
                                     </Box>
                                     <Button as='a' href='/' fontSize='14px' px='20px' bgGradient='linear(to-b, brand.200, brand.400)' color="white" _hover={{ bg: 'brand.200', color: 'white' }}>
