641cb23922c3723a2660af20e8b95900222dfa90
BrightTechnologies1 <silascyrax@gmail.com>
2023-09-12 22:13:56 +0100

    update: trading bot

--- src/components/botTrading/tradingbotCard.jsx
+++ src/components/botTrading/tradingbotCard.jsx
@@ -1,9 +1,9 @@
-import { Box, Flex, HStack, Popover, PopoverContent, PopoverTrigger, Stack, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, HStack, Image, Popover, PopoverContent, PopoverTrigger, Stack, Text, VStack } from '@chakra-ui/react'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 
 export default function TradingbotCard({ botTitle }) {
     return (
-        <VStack w='full' h='240px' bg='white' rounded='12px'>
+        <VStack w='full' gap='30px' bg='white' rounded='12px'>
             <HStack align='end' justify='space-between' w='full'>
                 <Box w='220px' color='white' roundedBottomRight='20px' px='30px' py='20px' bg='brand.500'>
                     <Text>{botTitle}</Text>
@@ -29,8 +29,42 @@ export default function TradingbotCard({ botTitle }) {
                     </Popover>
                 </Box>
             </HStack>
-            <Stack direction={['column', 'row']} justify='space-between' align='center'>
-
+            <Stack direction={['column', 'row']} justify='space-between' w='full' p='30px' align='center'>
+                <VStack align='left' gap='20px'>
+                    <HStack align='center' gap='20px'>
+                        <Image src='./closed.svg' alt='' />
+                        <VStack align='left'>
+                            <Text>Closed Trades</Text>
+                            <Text fontWeight={700}>5</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack align='center' gap='20px'>
+                        <Image src='./open.svg' alt='' />
+                        <VStack align='left'>
+                            <Text>Open Trades</Text>
+                            <Text fontWeight={700}>2</Text>
+                        </VStack>
+                    </HStack>
+                </VStack>
+                <VStack align='left' gap='20px'>
+                    <HStack align='center' gap='20px'>
+                        <Image src='./chartup.svg' alt='' />
+                        <VStack align='left'>
+                            <Text>Profits</Text>
+                            <Text fontWeight={700}>$10,234.00</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack align='center' gap='20px'>
+                        <Image src='./chartdown.svg' alt='' />
+                        <VStack align='left'>
+                            <Text>Loss</Text>
+                            <Text fontWeight={700}>$2,566.01</Text>
+                        </VStack>
+                    </HStack>
+                </VStack>
+                <VStack align='left' gap='20px'>
+                    <h1>chart here</h1>
+                </VStack>
             </Stack>
         </VStack>
     )
