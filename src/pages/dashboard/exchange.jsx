5b1835090781e0e68a93a7ec3e750339a3f7ec4b
BrightTechnologies1 <silascyrax@gmail.com>
2023-09-20 15:41:42 +0100

    update: exchange

--- src/pages/dashboard/exchange.jsx
+++ src/pages/dashboard/exchange.jsx
@@ -3,17 +3,17 @@ import { Box, Button, Divider, HStack, Heading, Image, Text, VStack } from '@cha
 export default function Exchange() {
     return (
         <VStack w='full' gap='50px'>
-            <VStack w='full' rounded='16px' p='30px' gap='12px'>
+            <VStack w='full' rounded='16px' bg='white' align='left' p='20px' gap='12px'>
                 <Heading>Connected Exchange Accounts</Heading>
                 <Text>Effortlessly manage your cryptocurrency exchange accounts and trade with ease using our integrated exchange hub.</Text>
             </VStack>
-            <VStack p='30px' w='full' bg='white'>
-                <HStack align='center' justify='space-between'>
+            <VStack p='30px' rounded='16px' gap='20px' w='full' bg='white'>
+                <HStack w='full' align='center' justify='space-between'>
                     <Box><Image src='./binance.svg' alt='' w='100px' /></Box>
-                    <Button variant='solid' px='26px' bg='none' _hover={{ bg: 'brand.200' }} w='fit-content'>Disconnect</Button>
+                    <Button variant='solid' border='1px' borderColor='brand.300' px='26px' bg='none' _hover={{ bg: 'brand.200' }} w='fit-content'>Disconnect</Button>
                 </HStack>
                 <Divider />
-                <HStack align='center' justify='space-between'>
+                <HStack w='full' align='center' justify='space-between'>
                     <Box><Image src='./binance.svg' alt='' w='100px' /></Box>
                     <Button variant='solid' px='26px' bgGradient='linear(to-b, brand.200, brand.400)' color='white' _hover={{ bg: 'brand.200' }} w='fit-content'>Get Started</Button>
                 </HStack>
